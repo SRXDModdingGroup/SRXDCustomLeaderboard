02d9775b7bd627b43c912c45e0de6ea61f52b28e
Amy Y <ctrlalternativedelete@gmail.com>
2022-10-06 10:15:46 +1100

    init

---
+++ SRXDCustomLeaderboard/Player_Custom.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using UnityEngine;
+
+namespace SRXDCustomLeaderboard;
+
+public class Player_Custom : UnityBaseProvider, IProvider, IServiceWithStates, IService
+{
+    public bool RequiresDataStore
+    {
+        get
+        {
+            return false;
+        }
+    }
+    
+    public int Priority
+    {
+        get
+        {
+            return 2;
+        }
+    }
+    public string _serviceName { get; set; }
+    
+    private string displayName;
+    private string customId;
+
+    private bool _initialized;
+    
+    public bool InitService()
+    {
+        return true;
+    }
+
+    public void UpdateService()
+    {
+    }
+
+    public void ShutdownService()
+    {
+    }
+    
+    public string GetDisplayName()
+    {
+        return this.displayName;
+    }
+
+    public bool IsSignedIn<T>() where T : class, IService
+    {
+        return this is T && this._initialized;
+    }
+
+    public void Login(LoginCallback callback)
+    {
+        PSMLoginResult psmloginResult = default(PSMLoginResult);
+        callback(psmloginResult);
+    }
+
+    public void Logout()
+    {
+    }
+
+    public void SetDisplayName(string displayName)
+    {
+    }
+}
---
+++ SRXDCustomLeaderboard/Plugin.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+using UnityEngine;
+
+namespace SRXDCustomLeaderboard
+{
+    [BepInPlugin("SRXDCustomLeaderboard", "SRXDCustomLeaderboard", "0.0.1.0")]
+    public class Plugin : BaseUnityPlugin
+    {
+        public static BepInEx.Logging.ManualLogSource Log;
+        private void Awake()
+        {
+            // Plugin startup logic
+            Log = Logger;
+            Log.LogInfo($"Plugin SRXDCustomLeaderboard is loaded!");
+            Harmony.CreateAndPatchAll(typeof(LeaderboardPatches));
+        }
+
+        private void Update()
+        {
+        }
+
+        private class LeaderboardPatches
+        {
+            [HarmonyPatch(typeof(Player_Steam), nameof(Player_Steam.DownloadLeaderboard))]
+            [HarmonyPostfix]
+            static void dlLeaderboardPost(string id, LeaderboardResult leaderboardToFill) {
+                Log.LogInfo($"{id}, {JsonUtility.ToJson(leaderboardToFill)}");
+            }
+             
+            // look into  SubmitTrackLeaderboard on how metadata is created LeaderboardSubmissionMetaData
+            [HarmonyPatch(typeof(Player_Steam), nameof(Player_Steam.SetLeaderboardValue))]
+            [HarmonyPrefix]
+            static void upLeaderboardPre(string key, LeaderboardSubmission submission) {
+                Log.LogInfo($"{key}, {JsonUtility.ToJson(submission)}");
+                // LeaderboardSubmissionMetaData: metadata field: [health, buildNumber, exeRevisionNumber, tracklistSize, _progress, score ^ 148089795, trackDataVersion, streak, fullComboState, tiebreakerScore]
+            }
+        }
+    }
+}
